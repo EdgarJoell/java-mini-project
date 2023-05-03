77d46370a289d36ede0df64a12ea384b74e05a79
Edgar Zambrana <edgarzambrana@Edgars-MacBook-Pro.local>
2023-05-03 01:24:52 -0400

    Complete game functionality

--- java-mini-project/src/main/java/Game.java
+++ java-mini-project/src/main/java/Game.java
@@ -2,6 +2,6 @@ public class Game {
     public static void main(String[] args) {
         MainMenu menu = new MainMenu();
 //        menu.play();
-        menu.setupPlayerGame();
+        menu.play();
     }
 }
--- java-mini-project/src/main/java/MainMenu.java
+++ java-mini-project/src/main/java/MainMenu.java
@@ -13,8 +13,14 @@ public class MainMenu {
     protected Scanner scan6 = new Scanner(System.in);
     protected ArrayList<String> history = new ArrayList<>();
 
+    Human playerOne = new Human("Player One", 0, "");
+
+    Human playerTwo = new Human("Player Two", 0, "");
+
+    Computer computer = new Computer("Computer", 0, "");
+
     public void play() {
-        System.out.println("Main Menu: \n\nPlay Game \nView History \nQuit \nPlease type either 'play', 'history', or 'quit'\n");
+        System.out.println("Main Menu: \n\nWelcome to the Rock, Paper, Scissors Game! \n\nPlay Game \nView History \nQuit \n\nPlease type either 'play', 'history', or 'quit'");
         takeMenuInput();
     }
 
@@ -24,22 +30,27 @@ public class MainMenu {
         return checkMenuInput(scanOne);
     }
 
+    /**
+     *
+     * @param input
+     * @return
+     */
     public String checkMenuInput(String input) {
         switch (input.toLowerCase()) {
             case "play" -> {
-                System.out.println("You chose to play game\nDo you wish to play 'player vs. player' or 'vs. computer'?\nPlease type either 'two' or 'computer'");
+                System.out.println("\nYou chose to play game\nDo you wish to play 'player vs. player' or 'vs. computer'?\nPlease type either 'two' or 'computer'");
                 humanOrComputer();
             }
             case "history" -> {
-                System.out.println("You chose to view history\n");
+                System.out.println("\nYou chose to view history\n");
                 viewHistory();
             }
             case "quit" -> {
-                System.out.println("You chose to quit. Hope to see you next time.");
+                System.out.println("\nYou chose to quit. Hope to see you next time.");
                 quitGame();
             }
             default -> {
-                System.out.println("You did not choose an correct option, please try again\n");
+                System.out.println("\nYou did not choose an correct option, please try again\n");
                 takeMenuInput();
             }
         }
@@ -64,11 +75,11 @@ public class MainMenu {
      */
     public String checkHumanOrComputer(String input) {
         if(input.toLowerCase().equals("two")) {
-            System.out.println("You chose to play 'player vs. player");
-            setupPlayerGame();
+            System.out.println("\nYou chose to play 'player vs. player");
+            setupPlayerGame(playerOne, playerTwo);
         } else if(input.toLowerCase().equals("computer")) {
-            System.out.println("You chose to play 'vs. computer");
-            setupComputerGame();
+            System.out.println("\nYou chose to play 'vs. computer");
+            setupComputerGame(playerOne, computer);
         }
         return input;
     }
@@ -77,27 +88,22 @@ public class MainMenu {
     /**
      * This method creates two Human player objects and asks each player in the console if they wish to change their name settings.
      */
-    public void setupPlayerGame() {
-        Human playerOne = new Human("Player One", 0, "");
-        Human playerTwo = new Human("Player Two", 0, "");
-
-        System.out.println(playerOne.getName() + ", would you like to change your name? Please type either 'yes' or 'no'");
+    public void setupPlayerGame(Human playerOne, Human playerTwo) {
+        System.out.println("\n" + playerOne.getName() + ", would you like to change your name? Please type either 'yes' or 'no'");
         String scanThree = scan3.nextLine();
         if(scanThree.toLowerCase().equals("yes")) {
             updatePlayerOne(playerOne);
         } else {
-            System.out.println("Player One's name is still Player One");
+            System.out.println("\nPlayer One's name is still Player One");
         }
 
-        System.out.println(playerTwo.getName() + ", would you like to change your name?");
+        System.out.println("\n|" + playerTwo.getName() + ", would you like to change your name?");
         scanThree = scan3.nextLine();
         if(scanThree.toLowerCase().equals("yes")) {
             updatePlayerTwo(playerTwo);
         } else {
-            System.out.println("Player Two's name is still Player Two");
+            System.out.println("\nPlayer Two's name is still Player Two");
         }
-        System.out.println("NEW GAME STARTING...\nARE YOU READY?...\nSHOOT\n" + playerOne.getName() + " it's your turn! Type either 'rock', 'paper', or 'scissors'");
-
         playGameWithPlayer(playerOne, playerTwo);
     }
 
@@ -106,10 +112,10 @@ public class MainMenu {
      * @param playerOne is the player object 'playerOne' passed down from the setupPlayerGame() method.
      */
     public void updatePlayerOne(Human playerOne) {
-        System.out.println("What would you like to change it to?");
+        System.out.println("\nWhat would you like to change it to?");
         String scanThree = scan3.nextLine();
         playerOne.setName(scanThree);
-        System.out.println("Player One's new name is " + playerOne.getName());
+        System.out.println("\nPlayer One's new name is " + playerOne.getName());
     }
 
     /**
@@ -117,35 +123,31 @@ public class MainMenu {
      * @param playerTwo is the player object 'playerTwo' passed down from the setupPlayerGame() method.
      */
     public void updatePlayerTwo(Human playerTwo) {
-        System.out.println("What would you like to change it to?");
+        System.out.println("\nWhat would you like to change it to?");
         String scanThree = scan3.nextLine();
         playerTwo.setName(scanThree);
-        System.out.println("Player Two's new name is " + playerTwo.getName());
+        System.out.println("\nPlayer Two's new name is " + playerTwo.getName());
     }
 
     /**
      * Creates one Human object and one Computer object and asks the player if they wish to change playerOne's name or the Computer's name.
      */
-    public void setupComputerGame() {
-        Human playerOne = new Human("Player One", 0, "");
-        Computer computer = new Computer("Computer", 0, "");
-
-        System.out.println(playerOne.getName() + ", would you like to change your name? Please type either 'yes' or 'no'");
+    public void setupComputerGame(Human player, Computer computer) {
+        System.out.println("\n" + playerOne.getName() + ", would you like to change your name? Please type either 'yes' or 'no'");
         String scanThree = scan3.nextLine();
         if(scanThree.toLowerCase().equals("yes")) {
             updatePlayerOne(playerOne);
         } else {
-            System.out.println("Player One's name is still Player One");
+            System.out.println("\nPlayer One's name is still Player One");
         }
 
-        System.out.println("Would you like to change the computers name?");
+        System.out.println("\nWould you like to change the computers name?");
         scanThree = scan3.nextLine();
         if(scanThree.toLowerCase().equals("yes")) {
             updateComputer(computer);
         } else {
-            System.out.println("Computer's name is still Computer");
+            System.out.println("\nComputer's name is still Computer");
         }
-        System.out.println("NEW GAME STARTING...\nARE YOU READY?...\nSHOOT\n" + playerOne.getName() + " it's your turn! Type either 'rock', 'paper', or 'scissors'");
         playGameWithComputer(playerOne, computer);
     }
 
@@ -154,19 +156,20 @@ public class MainMenu {
      * @param computer is the Computer object passed in from the setupComputerGame() method.
      */
     public void updateComputer(Computer computer) {
-        System.out.println("What would you like to change it to?");
+        System.out.println("\nWhat would you like to change it to?");
         String scanThree = scan3.nextLine();
         computer.setName(scanThree);
-        System.out.println("Player One's new name is " + computer.getName());
+        System.out.println("\nPlayer One's new name is " + computer.getName());
     }
 //-----------------------------------------------------------------------------------------------------------------------------------------------------
 
     public void playGameWithComputer(Human playerOne, Computer computer) {
+        System.out.println("\nNEW GAME STARTING...\nARE YOU READY?...\nSHOOT\n" + playerOne.getName() + " it's your turn! Type either 'rock', 'paper', or 'scissors'");
         takePlayerInput(playerOne);
         computerChooses(computer);
         System.out.println(playerOne.getName() + " chose " + playerOne.getInput() +" AND " + computer.getName() + " chose " + computer.getInput());
         if(playerOne.getInput().equals(computer.getInput())) {
-            System.out.println("It was a draw! Nobody wins.");
+            System.out.println("\nIt was a draw! Nobody wins.");
         } else if((playerOne.getInput() + computer.getInput()).equals("rockscissors") || (playerOne.getInput() + computer.getInput()).equals("paperrock") || (playerOne.getInput() + computer.getInput()).equals("scissorspaper")) {
             playerOne.setScore();
             System.out.println(playerOne.getName() + " won this round!\n" + "Their new score is " + playerOne.getScore());
@@ -175,7 +178,20 @@ public class MainMenu {
             computer.setScore();
             System.out.println(computer.getName() + " won this round!\n" + "Their new score is " + computer.getScore());
         }
-        
+        System.out.println("\nWould you like to play again, go to the main menu, or quit? Please type either 'again', 'menu', or 'quit'.");
+        String scan = scan1.nextLine();
+        if(scan.toLowerCase().equals("again")) {
+            playerOne.setInput("");
+            computer.setInput("");
+            playGameWithComputer(playerOne, computer);
+        } else if(scan.toLowerCase().equals("menu")) {
+            playerOne.setInput("");
+            computer.setInput("");
+            play();
+        } else {
+            quitGame();;
+        }
+        System.out.println("\n");
     }
 
     public void takePlayerInput(Human player) {
@@ -197,12 +213,13 @@ public class MainMenu {
     }
 
     public void playGameWithPlayer(Human playerOne, Human playerTwo) {
+        System.out.println("\nNEW GAME STARTING...\nARE YOU READY?...\nSHOOT\n" + playerOne.getName() + " it's your turn! Type either 'rock', 'paper', or 'scissors'");
         takePlayerInput(playerOne);
-        System.out.println(playerTwo.getName() + " it's your turn. Type either 'rock', 'paper', or 'scissors'");
+        System.out.println("\n" + playerTwo.getName() + " it's your turn. Type either 'rock', 'paper', or 'scissors'");
         takePlayerInput(playerTwo);
         System.out.println(playerOne.getName() + " chose " + playerOne.getInput() +" AND " + playerTwo.getName() + " chose " + playerTwo.getInput());
         if(playerOne.getInput().equals(playerTwo.getInput())) {
-            System.out.println("It was a draw! Nobody wins.");
+            System.out.println("\nIt was a draw! Nobody wins.");
             history.add(playerOne.getName() + " vs. " + playerTwo.getName() + "results. It was a draw");
         } else if((playerOne.getInput() + playerTwo.getInput()).equals("rockscissors") || (playerOne.getInput() + playerTwo.getInput()).equals("paperrock") || (playerOne.getInput() + playerTwo.getInput()).equals("scissorspaper")) {
             playerOne.setScore();
@@ -213,23 +230,44 @@ public class MainMenu {
             System.out.println(playerTwo.getName() + " won this round!\n" + "Their new score is " + playerTwo.getScore());
             history.add(playerOne.getName() + " vs. " + playerTwo.getName() + " results. " + playerTwo.getName() + " was the winner");
         }
+        System.out.println("\nWould you like to play again, go to the main menu, or quit? Please type either 'again', 'menu', or 'quit'.");
+        String scan = scan1.nextLine();
+        if(scan.toLowerCase().equals("again")) {
+            playerOne.setInput("");
+            playerTwo.setInput("");
+            playGameWithPlayer(playerOne, playerTwo);
+        } else if(scan.toLowerCase().equals("menu")) {
+            playerOne.setInput("");
+            playerTwo.setInput("");
+            play();
+        } else {
+            quitGame();;
+        }
+
+        System.out.println("\n");
     }
 
     public void checkPlayerValidity(Human player) {
         if(player.getInput().toLowerCase().equals("rock") || player.getInput().toLowerCase().equals("paper") || player.getInput().toLowerCase().equals("scissors")) {
-            System.out.println(player.getName() + " chooses " + player.getInput());
         } else {
-            System.out.println(player.getName() + " it appears you had a wrong input. Please try again");
+            System.out.println("\n" + player.getName() + " it appears you had a wrong input. Please try again");
             takePlayerInput(player);
         }
     }
+
     public void viewHistory() {
         for (String s : history) {
             System.out.println(s);
         }
+        System.out.println("\nType 'menu' to return back to the main menu.");
+        String scan = scan1.nextLine();
+        if(scan.toLowerCase().equals("menu")) {
+            play();
+        } else {
+            viewHistory();
+        }
     }
 
     public void quitGame() {
-
     }
 }
